74875420d6be0e000523327723bd1b28dc80d01f
Patrick Göler von Ravensburg <patrickvongoeler@gmail.com>
2021-08-05 16:31:23 +0200

    update landing text

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
             <h1 className="text-3xl md:text-5xl tracking-tight font-bold mb-4">Hello there 👋</h1>
             <p className="mb-16 text-secondary">
                 My name is Patrick and I'm a web developer currently finishing my master thesis at
-                the University of Mannheim. Additionally I work as a Software Engineer at Aioneers
-                👨‍💻.
+                the University of Mannheim in cooperation with SAP. Additionally I work as a
+                freelance Software Engineer 👨‍💻.
             </p>
 
             <Title variant="h2" className="mb-4">
